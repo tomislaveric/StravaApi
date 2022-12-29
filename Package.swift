c472855a2c189f58dcbe68910adf3dd448dd7c07
tomislav <hi@tomislav.mobi>
2022-12-29 22:21:50 +0100

    refactor code

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/tomislaveric/http-request.git", branch: "main"),
+         .package(url: "https://github.com/tomislaveric/oauth.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -24,13 +25,15 @@ let package = Package(
         .target(
             name: "StravaApi",
             dependencies: [
-                .product(name: "HTTPRequest", package: "http-request")
+                .product(name: "HTTPRequest", package: "http-request"),
+                .product(name: "OAuth", package: "oauth"),
             ]),
         .testTarget(
             name: "StravaApiTests",
             dependencies: [
                 "StravaApi",
-                .product(name: "HTTPRequest", package: "http-request")
+                .product(name: "HTTPRequest", package: "http-request"),
+                .product(name: "OAuth", package: "oauth"),
             ]),
     ]
 )
--- Sources/StravaApi/StravaApi.swift
+++ Sources/StravaApi/StravaApi.swift
@@ -1,40 +1,77 @@
 import HTTPRequest
 import Foundation
+import OAuth
 
 public protocol StravaApi {
-    func getProfile(token: String?) async throws -> Athlete
-    func getUserActivities(token: String?) async throws -> [DetailedActivity]
+    func getProfile() async throws -> Athlete
+    func getUserActivities() async throws -> [DetailedActivity]
 }
 
 public struct StravaApiImpl: StravaApi {
-    public func getUserActivities(token: String?) async throws -> [DetailedActivity] {
-        guard let url = URL(string: "\(Endpoint.baseUrl.rawValue)\(Endpoint.athleteActivities.rawValue)") else {
-            throw StravaApiError.badUrl
-        }
-        var urlRequest = URLRequest(url: url)
-        if let token = token {
-            urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
-        }
-        return try await session.get(request: urlRequest)
+    
+    public func getUserActivities() async throws -> [DetailedActivity] {
+        return try await handleRequest(endpoint: Endpoint.athleteActivities)
     }
     
+    public func getProfile() async throws -> Athlete {
+        return try await handleRequest(endpoint: Endpoint.athlete)
+    }
     
-    public func getProfile(token: String?) async throws -> Athlete {
-        guard let url = URL(string: "\(Endpoint.baseUrl.rawValue)\(Endpoint.athlete.rawValue)") else {
+    private func handleRequest<ReturnType: Decodable>(endpoint: Endpoint) async throws -> ReturnType {
+        guard let url = URL(string: "\(Endpoint.baseUrl.rawValue)\(endpoint.rawValue)") else {
             throw StravaApiError.badUrl
         }
         var urlRequest = URLRequest(url: url)
-        if let token = token {
+        if let token = try await oAuth.getAccessToken() {
             urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
         }
         return try await session.get(request: urlRequest)
     }
     
     private let session: HTTPRequest
+    private let oAuth: OAuth
     
-    public init(session: HTTPRequest = HTTPRequestImpl()) {
-        self.session = session
+    public init(config: StravaConfig) {
+        let oAuthConfig = OAuthConfig(
+            authorizeUrl: config.authorizeUrl,
+            tokenUrl: config.tokenUrl,
+            clientId: config.clientId,
+            redirectUri: config.redirectUri,
+            callbackURLScheme: config.callbackURLScheme,
+            clientSecret: config.clientSecret,
+            scope: config.scope.joined(separator: ","))
+        
+        oAuth = OAuthImpl(config: oAuthConfig)
+        session = HTTPRequestImpl()
     }
+}
+
+public struct StravaConfig {
+    public init(authorizeUrl: String, tokenUrl: String, clientId: String, redirectUri: String, callbackURLScheme: String, clientSecret: String, scope: [String]) {
+        self.authorizeUrl = authorizeUrl
+        self.tokenUrl = tokenUrl
+        self.clientId = clientId
+        self.redirectUri = redirectUri
+        self.callbackURLScheme = callbackURLScheme
+        self.clientSecret = clientSecret
+        self.scope = scope
+    }
+    
+    public enum GrantType: String {
+        case authorizationCode = "authorization_code"
+        case refreshToken = "refresh_token"
+    }
+    
+    public let authorizeUrl: String
+    public let tokenUrl: String
+    public let clientId: String
+    public let redirectUri: String
+    public let callbackURLScheme: String
+    public let clientSecret: String
+    public let responseType: String = "code"
+    public let approvalPrompt: String = "auto"
+    public let grantType: GrantType = .authorizationCode
+    public let scope: [String]
 }
 
 enum Endpoint: String {
