eebac228024f85e52870b0b4f36e073fd792a3ed
tomislav <hi@tomislav.mobi>
2022-12-29 11:50:36 +0100

    extend functions with token as parameter

--- Sources/StravaApi/StravaApi.swift
+++ Sources/StravaApi/StravaApi.swift
@@ -2,35 +2,37 @@ import HTTPRequest
 import Foundation
 
 public protocol StravaApi {
-    func getProfile() async throws -> Athlete
-    func getUserActivities() async throws -> [DetailedActivity]
+    func getProfile(token: String?) async throws -> Athlete
+    func getUserActivities(token: String?) async throws -> [DetailedActivity]
 }
 
 public struct StravaApiImpl: StravaApi {
-    public func getUserActivities() async throws -> [DetailedActivity] {
+    public func getUserActivities(token: String?) async throws -> [DetailedActivity] {
         guard let url = URL(string: "\(Endpoint.baseUrl.rawValue)\(Endpoint.athleteActivities.rawValue)") else {
             throw StravaApiError.badUrl
         }
         var urlRequest = URLRequest(url: url)
-        urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+        if let token = token {
+            urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+        }
         return try await session.get(request: urlRequest)
     }
     
     
-    public func getProfile() async throws -> Athlete {
+    public func getProfile(token: String?) async throws -> Athlete {
         guard let url = URL(string: "\(Endpoint.baseUrl.rawValue)\(Endpoint.athlete.rawValue)") else {
             throw StravaApiError.badUrl
         }
         var urlRequest = URLRequest(url: url)
-        urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+        if let token = token {
+            urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+        }
         return try await session.get(request: urlRequest)
     }
     
-    private let token: String
     private let session: HTTPRequest
     
-    public init(token: String, session: HTTPRequest = HTTPRequestImpl()) {
-        self.token = token
+    public init(session: HTTPRequest = HTTPRequestImpl()) {
         self.session = session
     }
 }
